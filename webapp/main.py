561c47da6f274ddad16a82bd509495df66162baf
Alfredo Deza <alfredo@deza.pe>
2024-01-28 20:00:28 -0500

    Update main.py

--- webapp/main.py
+++ webapp/main.py
@@ -1,8 +1,6 @@
-from transformers import pipeline
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
 
-generator = pipeline('text-generation', model='gpt2')
 
 app = FastAPI()
 
@@ -13,10 +11,9 @@ class Body(BaseModel):
 
 @app.get('/')
 def root():
-    return Response("<h1>A self-documenting API to interact with a GPT2 model and generate text</h1>")
+    return Response("<h1>A self-documenting API that uses the FastAPI framework</h1>")
 
 
 @app.post('/generate')
 def predict(body: Body):
-    results = generator(body.text, max_length=35, num_return_sequences=1)
-    return results[0]
+    return {"text": body.text}
